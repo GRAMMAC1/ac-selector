f98f9643a771d8618b35950c9a20a90f4d43cbee
fox <zhouxiaocongmail@foxmail.com>
2020-04-06 16:36:27 +0800

    rebuild

--- build/utils.js
+++ build/utils.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -12,13 +14,13 @@ var _react = _interopRequireDefault(require("react"));
 
 var _tinper = require("./components/tinper");
 
+var langs = _interopRequireWildcard(require("./lang"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
@@ -28,7 +30,16 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-var SubMenu = _tinper.Menu.SubMenu; // 重置_checked属性为false && 为表格添加key
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var SubMenu = _tinper.Menu.SubMenu;
+
+var i18n = _objectSpread({}, langs); // 重置_checked属性为false && 为表格添加key
+
 
 function resetChecked(list, type) {
   if (!Array.isArray(list)) {
@@ -65,15 +76,20 @@ function setChecked(source, ref, type) {
 
 var addFullAttr = function addFullAttr() {
   var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+  var local = arguments.length > 1 ? arguments[1] : undefined;
   var res = data.map(function (t, i) {
     return {
       key: t.key ? t.key : "userid-".concat(i),
       _checked: t._checked,
       userid: t.userid ? t.userid : "userid-".concat(i),
-      username: t.username ? t.username : '未知姓名',
-      email: t.email ? t.email : '未知邮箱',
-      mobile: t.mobile ? t.mobile : '未知号码',
-      orgName: t.orgName ? t.orgName : '未知部门'
+      username: t.username ? t.username : i18n[local].unknName,
+      email: t.email ? t.email : i18n[local].unknEmail,
+      mobile: t.mobile ? t.mobile : i18n[local].unknNo,
+      orgName: t.orgName ? t.orgName : i18n[local].unknPart // username: t.username ? t.username : '未知姓名',
+      // email: t.email ? t.email : '未知邮箱',
+      // mobile: t.mobile ? t.mobile : '未知号码',
+      // orgName: t.orgName ? t.orgName : '未知部门',
+
     };
   });
   return res;
